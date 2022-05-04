bf1cd7d15f5bb82ddc61541335d5ee9cf06b2607
zhentaozhang <321799910@qq.com>
2022-05-04 20:44:27 +0800

    Initial Commit

---
+++ pages/issue/issue.js
@@ -0,0 +1,91 @@
+// pages/issue/issue.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        show: false,
+        action: [{
+                name: "选项"
+            },
+            {
+                name: "分享",
+                subname: "描述信息",
+                openType: "share"
+            },
+            {
+                locding: true
+            },
+            {
+                name: "禁用选项",
+                disabled: true
+            },
+        ]
+    },
+    onClose() {
+        this.setData({
+            show: false
+        });
+    },
+
+    onSelect(event) {
+        console.log(event.detail);
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
